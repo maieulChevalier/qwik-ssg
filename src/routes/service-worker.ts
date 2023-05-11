9feadc6c35ea8243e600ed3acae310921d869192
maieulchevalier <maieul.chevalier@gmail.com>
2023-05-11 15:48:22 +0200

    fix pwa #2

--- src/routes/service-worker.ts
+++ src/routes/service-worker.ts
@@ -11,32 +11,8 @@ import { setupServiceWorker } from "@builder.io/qwik-city/service-worker";
 
 setupServiceWorker();
 
-const CACHE_NAME = "cache-test";
-
 addEventListener("install", () => self.skipWaiting());
 
 addEventListener("activate", () => self.clients.claim());
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request).then((response) => {
-        if (!response || response.status !== 200 || response.type !== "basic") {
-          return response;
-        }
-
-        const responseToCache = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseToCache);
-        });
-
-        return response;
-      });
-    })
-  );
-});
 
 declare const self: ServiceWorkerGlobalScope;
